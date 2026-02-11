0d7fca101d2eb733d80197494d51c33de0624f16
hilasolo <>
2026-02-11 15:04:35 +0000

    Combining Conditions

---
+++ conditional_statements/combining_conditions/main.py
@@ -0,0 +1,9 @@
+# The item's discount and stock status have been defined
+discounted = False
+lowStock = True
+movingProduct = discounted or lowStock
+#promotion = discounted and lowStock
+promotion = not (discounted or lowStock)
+
+
+print ("Is the item eligible for promotion?", promotion )
